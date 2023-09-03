38247b7b5e5ad07b858017b566d803375f5fbd85
Dennis <57582915+Dennis824@users.noreply.github.com>
2023-09-03 17:02:11 -0600

    feat (mapper) added RecipientMapper

---
+++ src/main/java/com/example/awssqs/mapper/RecipientMapper.java
@@ -0,0 +1,15 @@
+package com.example.awssqs.mapper;
+
+import com.example.awssqs.dto.RecipientDto;
+import com.example.awssqs.entity.RecipientEntity;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface RecipientMapper {
+
+    RecipientDto map(RecipientEntity entity);
+
+    @InheritInverseConfiguration
+    RecipientEntity map(RecipientDto dto);
+}
